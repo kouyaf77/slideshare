0192c3a9e918e0be99f4f1031c7e3558ebb332b9
dxhuy1988 <doxuanhuy@gmail.com>
2013-10-09 21:24:25 +0900

    remove init.rb uncessary code

--- init.rb
+++ init.rb
@@ -1,6 +1,1 @@
-$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/lib')
 require "slideshare"
-
-slideshare = SlideShare::Base.new(api_key: 'cnl87Hvt', shared_secret: 'wLnpvO51')
-ret = slideshare.slideshows.find_all_by_user('dxhuy88')
-p ret
